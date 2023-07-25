ceda8ad1a838844c2f251ebe6cb1f0e7fece8b85
TarasDAmc <tdzivik@amctech.com>
2023-07-25 16:57:33 +0200

    Added basic login model form and the Login page (basic creation)

---
+++ Model/LoginFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PocketManager.Model;
+public class LoginFormModel
+{
+    [Display(Prompt = "example@mail.com", Name = "Email")]
+    public string Email { get; set; }
+
+    [Display(Name = "Password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
---
+++ Veiw/Pages/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PocketManager.Veiw.Pages;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
