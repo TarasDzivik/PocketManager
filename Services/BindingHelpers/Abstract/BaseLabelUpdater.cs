fe40d76a63d11ac7ca4fb9df68c8ab0db525d831
TarasDAmc <tdzivik@amctech.com>
2023-08-04 15:36:05 +0200

    Added BaseLabelUpdater for future Label manipulations.

---
+++ Services/BindingHelpers/Abstract/BaseLabelUpdater.cs
@@ -0,0 +1,7 @@
+﻿namespace PocketManager.Services.BindingHelpers.Abstract;
+public abstract class BaseLabelUpdater<T> where T : Label
+{
+    public static void TextState(T element, string stateText) => element.Dispatcher.Dispatch(() => element.Text = stateText);
+    public static void TextColorState(T element, Color color) => element.Dispatcher.Dispatch(() => element.TextColor = color);
+    public static void FontSizeState(T element, double size) => element.Dispatcher.Dispatch(() => element.FontSize = size);
+}
